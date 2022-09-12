517c90a0c2b5094bb1318239e078fb7c24e64bfe
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-09-12 15:45:13 +0300

    Tehtävä 2b valmis

--- Main.java
+++ Main.java
@@ -5,5 +5,11 @@ class Main {
         for(int i=0; i<10; i++) {
             System.out.println(i);
         }
+
+        // Tehtava 2 b: tulosta luvut 10 - 1
+
+        for(int i=10; i>=1; i--) {
+            System.out.println(i);
+        }
     }
 }
