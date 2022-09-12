e2b9f59ee494703a1bd60b65be728495dc139300
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-09-12 15:43:06 +0300

    Sain for-luupin toimimaan.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
     public static void main(String [] args) {
         System.out.println("hello world");
+
+        for(int i=0; i<10; i++) {
+            System.out.println(i);
+        }
     }
 }
